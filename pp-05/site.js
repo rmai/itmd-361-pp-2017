79bc8c359edf79f05722b5acdc6cb2e35438ecc9
Randy Mai <randymai@yahoo.com>
2017-11-07 17:04:43 -0600

    made when java is active remove play bar but doesnt work vice versa

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,4 +1,16 @@
-function ringDoorbell() {
-  var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
+  //function ringDoorbell() {
+    //var doorbell = new Audio('media/doorbell.mp3');
+    //doorbell.play();
+  //}
+$.noConflict();
+
+    document.getElementById('ringDoorbell').style.visibility = 'hidden';
+  (function ($){
+    document.getElementById('ringDoorbell').style.visibility = 'visible';
+
+    var ringDoorbell = new Audio('media/doorbell.mp3');
+    document.getElementById('audio').style.visibility = 'hidden';
+    $('#ringDoorbell').on('click', function() {
+      ringDoorbell.play();
+    });
+  })(jQuery);
