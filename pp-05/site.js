6862ffbadd437e98699c3af002f3684e63aefe2d
Randy Mai <randymai@yahoo.com>
2017-11-13 11:31:16 -0600

    removed unneede tags

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,10 +1,8 @@
 $.noConflict();
 
-document.getElementById('ringDoorbell').style.visibility = 'hidden';
-
 (function ($){
   var ringDoorbell = new Audio('media/doorbell.mp3');
-  document.getElementById('ringDoorbell').style.visibility = 'visible';
+  // The page loads with js play bar will disapper
   document.getElementById('audio').style.visibility = 'hidden';
   $('#ringDoorbell').on('click', function() {
     ringDoorbell.play();
