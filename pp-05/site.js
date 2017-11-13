fec1b04830663461275525dc32b0a7a95631f926
Randy Mai <randymai@yahoo.com>
2017-11-13 11:20:11 -0600

    fix linting error

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,16 +1,10 @@
-  //function ringDoorbell() {
-    //var doorbell = new Audio('media/doorbell.mp3');
-    //doorbell.play();
-  //}
 $.noConflict();
-
-    document.getElementById('ringDoorbell').style.visibility = 'hidden';
-  (function ($){
-    document.getElementById('ringDoorbell').style.visibility = 'visible';
-
-    var ringDoorbell = new Audio('media/doorbell.mp3');
-    document.getElementById('audio').style.visibility = 'hidden';
-    $('#ringDoorbell').on('click', function() {
-      ringDoorbell.play();
-    });
-  })(jQuery);
+document.getElementById('ringDoorbell').style.visibility = 'hidden';
+(function ($){
+  var ringDoorbell = new Audio('media/doorbell.mp3');
+  document.getElementById('ringDoorbell').style.visibility = 'visible';
+  document.getElementById('audio').style.visibility = 'hidden';
+  $('#ringDoorbell').on('click', function() {
+    ringDoorbell.play();
+  });
+})(jQuery);
