49001487b28075286a853b64581804c2902a1244
Randy Mai <randymai@yahoo.com>
2017-11-13 21:23:58 -0600

    realized my intentions in my old js can be revised to make it look simpler by doing a different method in js

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,10 +1,14 @@
-$.noConflict();
+$(document).ready(function(){
 
-(function ($){
-  var ringDoorbell = new Audio('media/doorbell.mp3');
-  // The page loads with js play bar will disapper
-  document.getElementById('audio').style.visibility = 'hidden';
-  $('#ringDoorbell').on('click', function() {
-    ringDoorbell.play();
+  $("#audio").remove();
+
+  function ringDoorbell() {
+    var audio = new Audio('media/doorbell.mp3');
+    audio.play();
+  }
+
+  $("#doorbell").on("click", function() {
+    ringDoorbell();
   });
-})(jQuery);
+
+});
